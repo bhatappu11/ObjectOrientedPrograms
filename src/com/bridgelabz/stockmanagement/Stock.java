fe031dcb76c6c6b250ade1168be6265e04d34383
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-05 21:38:51 +0530

    MODIFIED: getters() and setters() created for initalising variables of stock

--- src/com/bridgelabz/stockmanagement/Stock.java
+++ src/com/bridgelabz/stockmanagement/Stock.java
@@ -1,5 +1,31 @@
 package com.bridgelabz.stockmanagement;
 
 public class Stock {
-
+	private String stockName;
+	private	Integer numberOfShares;
+	private Double sharePrice;
+	public Stock(String stockName, Integer numberOfShares, Double sharePrice) {
+		this.stockName = stockName;
+		this.numberOfShares = numberOfShares;
+		this.sharePrice = sharePrice;
+	}
+	public String getStockName() {
+		return stockName;
+	}
+	public void setStockName(String stockName) {
+		this.stockName = stockName;
+	}
+	public Integer getNumberOfShares() {
+		return numberOfShares;
+	}
+	public void setNumberOfShares(Integer numberOfShares) {
+		this.numberOfShares = numberOfShares;
+	}
+	public Double getSharePrice() {
+		return sharePrice;
+	}
+	public void setSharePrice(Double sharePrice) {
+		this.sharePrice = sharePrice;
+	}	
+	
 }
