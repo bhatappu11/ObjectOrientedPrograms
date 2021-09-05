522da660f2460748e76634ddb8cfd400d9a5c67b
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-05 22:29:15 +0530

    ADDED: CompanyShares class to maintain company shares

---
+++ src/com/bridgeabz/commercialdataprocessing/CompanyShares.java
@@ -0,0 +1,9 @@
+package com.bridgeabz.commercialdataprocessing;
+
+import java.time.LocalDateTime;
+
+public class CompanyShares {
+	private String symbol;
+	private int noOfShares;
+	private LocalDateTime dataTime;
+}
--- src/com/bridgeabz/commercialdataprocessing/StockAccount.java
+++ src/com/bridgeabz/commercialdataprocessing/StockAccount.java
@@ -1,12 +1,13 @@
 package com.bridgeabz.commercialdataprocessing;
 
 public class StockAccount {
+	private CompanyShares[] companyShares;
 	public StockAccount(String filename){
-		
+		companyShares = new CompanyShares[20];
 	}
 	
 	public double valueOf() {
-		
+		return 0;
 	}
 	public void buy(int amount,String symbol) {
 		
