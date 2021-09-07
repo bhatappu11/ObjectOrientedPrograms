04051a0e1eca97623ed8e8cc64a272994bfedcca
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-07 06:55:07 +0530

    REFACTOR: edited main class to create InventoryManager object

--- src/com/bridgelabz/inventorymanagement/InventoryMain.java
+++ src/com/bridgelabz/inventorymanagement/InventoryMain.java
@@ -1,23 +1,12 @@
 package com.bridgelabz.inventorymanagement;
 
-import com.bridgelabz.linkedlistfiles.INode;
-import com.bridgelabz.linkedlistfiles.LinkedList;
-import com.bridgelabz.linkedlistfiles.*;
-
 public class InventoryMain {
 
 	public static void main(String[] args) {
-		Inventory rice=new Inventory("Rice",50.0, 10.5);
-		Inventory wheat=new Inventory("Wheat",25.0, 25.0);
-		Inventory pulses=new Inventory("Pulses",15.5, 23.5);
-		LinkedList<Inventory> inventoryList=new LinkedList<>();
-		inventoryList.add(new Node<Inventory>(rice));
-		inventoryList.add(new Node<Inventory>(wheat));
-		inventoryList.add(new Node<Inventory>(pulses));
-		rice.valueOfInventory();
-		wheat.valueOfInventory();
-		pulses.valueOfInventory();
-		inventoryList.printNodes();
+		InventoryManager inventoryManager = new InventoryManager();
+		inventoryManager.readInventory();
+		inventoryManager.valueOfInventory();
+		inventoryManager.printInventory();
 	}
 
 }
